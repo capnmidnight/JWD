193dc956c8933408b4c12a27982a4a24aa69a59c
Sean T. McBeth <sean.mcbeth@gmail.com>
2014-03-27 23:41:14 -0400

    word frequency analysis

--- html5/dammit.js
+++ html5/dammit.js
@@ -11,7 +11,10 @@ var header = null,
     totalWordCount = null,
     addWordCount = null,
     clock = null,
-    notes = null,
+    wordFrequency = null,
+    word2Frequency = null,
+    word3Frequency = null,
+    word4Frequency = null,
     browserInfo = null,
 
     files = null,
@@ -31,7 +34,10 @@ function getControls(){
     totalWordCount = getDOM("#total-word-count");
     addWordCount = getDOM("#add-word-count");
     clock = getDOM("#clock");
-    notes = getDOM("#notes");
+    wordFrequency = getDOM("#word-frequency");
+    word2Frequency = getDOM("#word-2-frequency");
+    word3Frequency = getDOM("#word-3-frequency");
+    word4Frequency = getDOM("#word-4-frequency");
     browserInfo = getDOM("#browser-info");
 
     menuItems.forEach(function (menuItem) {
@@ -66,13 +72,35 @@ function resize(){
         - 5);
 }
 
+function frequency(arr) {
+    var result = arr
+        .group()
+        .map(function (word) { return [word[0], word[1].length]; })
+        .group(function (word) { return word[1]; }, function (word) { return word[0]; })
+        .map(function (count) {
+            count[1].sort();
+            return [count[0], count[1].join(", ")];
+        })
+        .filter(function(count){
+            return count[0] > 1
+        });
+
+    result.sort(function (a, b) {
+        return b[0] - a[0];
+    });
+
+    return result.map(function (word) {
+        return fmt("$1: $2", word[0], word[1]);
+    }).join("\n<br>");
+}
+
 function countWords(){
     var words = editor.getValue()
         .replace(/<\/?(br|p)>/g, "\n")
         .replace(/<[^>]+>/g, "")
         .replace(/&nbsp;/g, " ")
-        .match(/[\w']+/g),
-        count = 0, counts = [];
+        .match(/[\w'’]+/g),
+        count = 0, counts = "", counts2 = "", counts3 = "";
 
     if(words != null){
         words = words.map(function(word){
@@ -80,19 +108,19 @@ function countWords(){
         });
 
         count = words.length;
-
-        counts = words
-            .group()
-            .map(function(word){ return [word[0], word[1].length]; })
-            .group(function(word){ return word[1]; }, function(word){ return word[0]; })
-            .map(function(count){
-                count[1].sort();
-                return [count[0], count[1].join(", ")];
-            });
-
-        counts.sort(function(a, b){
-            return b[0] - a[0];
-        });
+        counts = frequency(words);
+        var words2 = [], words3 = [], words4 = [];
+        for (var i = 0; i < words.length; ++i) {
+            if (i > 0)
+                words2.push(words.slice(i - 1, i + 1).join(" "));
+            if (i > 1)
+                words3.push(words.slice(i - 2, i + 1).join(" "));
+            if (i > 2)
+                words4.push(words.slice(i - 3, i + 1).join(" "));
+        }
+        counts2 = frequency(words2);
+        counts3 = frequency(words3);
+        counts4 = frequency(words4);
     }
 
     if(!files[currentFile].count)
@@ -101,9 +129,10 @@ function countWords(){
     totalWordCount.setValue(count);
     addWordCount.setValue(count - files[currentFile].count);
 
-    notes.setValue(counts.map(function(word){
-        return fmt("$1: $2", word[0], word[1]);
-    }).join("\n<br>"));
+    wordFrequency.setValue(counts);
+    word2Frequency.setValue(counts2);
+    word3Frequency.setValue(counts3);
+    word4Frequency.setValue(counts4);
 }
 
 function clockTick(){
@@ -125,8 +154,8 @@ function pageLoad(){
 
     var dataLoaded = loadData();
 
-    menuItems[dataLoaded ? 0 : menuItems.length - 1].click();
-    //menuItems[5].click();
+    //menuItems[dataLoaded ? 0 : menuItems.length - 1].click();
+    menuItems[1].click();
 
     editor.addEventListener("keyup", interrobang, false);
     editor.addEventListener("keyup", countWords, false);
