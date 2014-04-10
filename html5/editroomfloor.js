15836c97c618bad3535bd51105edbad45b27596f
Sean T. McBeth <sean.mcbeth@gmail.com>
2014-04-10 11:04:35 -0400

    placeholder for drag-and-drop editing

--- html5/editroomfloor.js
+++ html5/editroomfloor.js
@@ -1,57 +1,64 @@
-﻿var selectedWritingBlock  = null;
+﻿var selectedWritingBlock = null;
 
-function buildEditView(){
-    var doc = data.chapters[currentChapter].doc; 
+function buildEditView() {
+    var doc = data.chapters[currentChapter].doc;
+
     doc = doc.replace(/\r\n/g, "\n"); // normalize newl ine cha racters
     doc = doc.split(/\n\n/g)
-        .map(function(para){
+        .map(function (para) {
             para = para.replace(/([.…!?‽])(\s+)/g, "$1</span>$2<span class=\"sentence\">");
-            return fmt("<p class=\"paragraph\"><span class=\"sentence\">$1</span></p>",  para);
+            return fmt("<p class=\"paragraph\"><span class=\"sentence\">$1</span></p>", para);
         })
         .join("\n");
     editor.setValue(doc);
-    getDOMAll(".sentence ").forEach(function(s ){
-        s.addEventListener("mousedown", pickupWriting);
-        s.addEventListener("mouseup", placeWriting);
-        s.addEventListener("mousemove", moveWriting);
+    getDOMAll(".sentence ").forEach(function (s) {
+        s.addEventListener(isMobile ? "touchstart" : "mousedown", pickupWriting);
+        s.addEventListener(isMobile ? "touchend" : "mouseup", placeWriting);
+        s.addEventListener(isMobile ? "touchmove" : "mousemove", moveWriting);
     });
-    getDOMAll(".paragraph").forEach(function(s){
-        s.addEventListener("mousedown", pickupWriting);
-        s.addEventListener("mouseup", placeWriting);
-        s.addEventListener("mousemove", moveWriting);
+    getDOMAll(".paragraph").forEach(function (s) {
+        s.addEventListener(isMobile ? "touchstart" : "mousedown", pickupWriting);
+        s.addEventListener(isMobile ? "touchend" : "mouseup", placeWriting);
+        s.addEventListener(isMobile ? "touchmove" : "mousemove", moveWriting);
     });
 }
 
-function getLocation(box){
+function getLocation(box) {
     var loc = [0, 0];
-    for(var i = box; !!i; i = i.parentElement){
+    for (var i = box; !!i; i = i.parentElement) {
         loc[0] += i.offsetLeft;
         loc[1] += i.offsetTop;
     }
     return loc;
 }
+
 var delta = [0, 0];
-function pickupWriting(evt){
-    selectedWritingBlock = this;
-    var loc = [this.offsetLeft, this.offsetTop];
-    this.style.position = "absolute";
-    this.style.left = px(loc[0]);
-    this.style.top = px(loc[1]);
-    delta[0] = loc[0] - evt.pageX;
-    delta[1] = loc[1] - evt.pageY;
-    var parent = this.parentElement;
-    if(parent != editor){
-        parent.removeChild(this);
-        editor.appendChild(this);
-        if(parent.children.length == 0)
-            editor.removeChild(parent);
+
+function pickupWriting(evt) {
+    if(isMobile)
+        evt = evt.touches[0];
+    if(this.className == "sentence"){
+        selectedWritingBlock = this;
+        var loc = [this.offsetLeft, this.offsetTop];
+        this.style.position = "absolute";
+        this.style.left = px(loc[0]);
+        this.style.top = px(loc[1]);
+        delta[0] = loc[0] - evt.pageX;
+        delta[1] = loc[1] - evt.pageY;
+        var parent = this.parentElement;
+        if (parent != editor) {
+            parent.removeChild(this);
+            editor.appendChild(this);
+            if (parent.children.length == 0)
+                editor.removeChild(parent);
+        }
+        evt.preventDefault();
+        evt.stopPropagation();
     }
-    evt.preventDefault();
-    evt.stopPropagation();
 }
 
-function getInsertPosition(){
-    if(selectedWritingBlock && over){
+function getInsertPosition() {
+    if (selectedWritingBlock && over) {
         var dx1 = selectedWritingBlock.offsetLeft - over.offsetLeft;
         var dy1 = selectedWritingBlock.offsetTop - over.offsetTop;
         var dx2 = dx1 - over.offsetWidth;
@@ -63,24 +70,31 @@ function getInsertPosition(){
     }
 }
 
-function placeWriting(){
-    if(selectedWritingBlock && over){
+function placeWriting() {
+    if (selectedWritingBlock && over) {
         selectedWritingBlock.style.position = "";
         selectedWritingBlock.style.left = "";
         selectedWritingBlock.style.top = "";
-        editor.removeChild(selectedWritingBlock); 
-        if(selectedWritingBlock.className == over.className)
+        editor.removeChild(selectedWritingBlock);
+        if (selectedWritingBlock.className == over.className)
             over.insertAdjacentHTML(getInsertPosition(), selectedWritingBlock.outerHTML);
         else
             over.appendChild(selectedWritingBlock);
         selectedWritingBlock = null;
+        over = null;
+
+        var placeholder = getDOM("#editor-placeholder");
+        if(placeholder)
+            placeholder.parentElement.removeChild(placeholder);
+
         var doc = editor.innerHTML;
         doc = doc
-            .replace(/\n/g,"")
+            .replace(/\n/g, "")
             .replace(/<\/p>/g, "\n\n")
             .replace(/<[^>]+>/g, " ")
             .replace(/ +/g, " ")
-            .replace(/\n /g, "\n");
+            .replace(/\n /g, "\n")
+            .trim();
 
         data.chapters[currentChapter].doc = doc;
         autoSave();
@@ -89,10 +103,31 @@ function placeWriting(){
 }
 
 var over = null;
-function moveWriting(evt){
-    if(selectedWritingBlock){
+function moveWriting(evt) {
+    if(isMobile)
+        evt = evt.touches[0];
+
+    if (selectedWritingBlock) {
         selectedWritingBlock.style.left = px(evt.pageX + delta[0]);
         selectedWritingBlock.style.top = px(evt.pageY + delta[1]);
-        over = document.elementFromPoint(selectedWritingBlock.offsetLeft - 2, selectedWritingBlock.offsetTop);
+        var newOver = document.elementFromPoint(selectedWritingBlock.offsetLeft - 2, selectedWritingBlock.offsetTop);
+        if(newOver && newOver.id != "editor-placeholder")
+            over = newOver;
+        if(over){
+            var placeholder = getDOM("#editor-placeholder");
+            if(placeholder)
+                placeholder.parentElement.removeChild(placeholder);
+
+            placeholder = document.createElement(selectedWritingBlock.tagName);
+            placeholder.className = selectedWritingBlock.className;
+            placeholder.innerHTML = selectedWritingBlock.innerHTML;
+            placeholder.id = "editor-placeholder";
+            placeholder.style.visibility = "hidden";
+        
+            if (placeholder.className == over.className)
+                over.insertAdjacentHTML(getInsertPosition(), placeholder.outerHTML);
+            else
+                over.appendChild(placeholder);
+        }
     }
 }
