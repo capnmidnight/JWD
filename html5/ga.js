2cb95d699b01198611688114a136bc61e532b229
Sean T. McBeth <sean.mcbeth@gmail.com>
2014-04-17 09:08:05 -0400

    new analytics events

--- html5/ga.js
+++ html5/ga.js
@@ -2,40 +2,17 @@
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
 ga('create', 'UA-49938410-1', 'justwritedammit.com');
+ga('require', 'linkid', 'linkid.js')
 ga('require', 'displayfeatures');
 ga('send', 'pageview');
 
-var conversionLabels = {
-    save: "6hMOCNCPuwgQqPnO1QM",
-    load: "0EO6CLiSuwgQqPnO1QM",
-    saveSnip: "GzTCCKCVuwgQqPnO1QM",
-    analysisFreq: "ae9-COCcuwgQqPnO1QM",
-    linkDorpbox: "Fk2xCMCRuwgQqPnO1QM",
-    linkGDrive: "fnEZCKCkuwgQqPnO1QM",
-    mailingList: "gMfZCICouwgQqPnO1QM",
-    reset: "jWx5CLCTuwgQqPnO1QM"
-};
-
-function goog_snippet_vars(type) {
-    var w = window;
-    w.google_conversion_id = 984857768;
-    w.google_conversion_label = conversionLabels[type];
-    w.google_remarketing_only = false;
+function evtIt(cat, act, lbl, val){
+    ga("send", "event", cat, act, lbl, val);
 }
 
-function goog_report_conversion(type, url) {
-    goog_snippet_vars(type);
-    window.google_conversion_format = "3";
-    window.google_is_call = true;
-    var opt = new Object();
-    opt.onload_callback = function() {
-        if(typeof(url) != "undefined")
-            document.location = url;
-    };
-    var conv_handler = window['google_trackConversion'];
-    if (typeof(conv_handler) == 'function') {
-        conv_handler(opt);
-    }
-}
+var navIt = evtIt.bind(window, "button", "click");
+var datIt = evtIt.bind(window, "data");
+var savIt = datIt.bind(window, "save");
+var lodIt = datIt.bind(window, "load");
+var usrIt = evtIt.bind(window, "user");
